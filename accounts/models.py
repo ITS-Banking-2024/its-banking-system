d47038cade9275758d220232b4b7f2600df158d0
rgligora <roko.gligora@gmail.com>
2024-11-25 18:21:15 +0100

    Refactor account on_delete_customer to PROTECT

--- accounts/models.py
+++ accounts/models.py
@@ -9,7 +9,7 @@ class AccountBase(Account):
 
     objects : AccountManager = AccountManager()
 
-    customer_id = models.ForeignKey(CUSTOMER_MODEL, related_name='accounts', on_delete=models.CASCADE, null=True, blank=True)
+    customer_id = models.ForeignKey(CUSTOMER_MODEL, related_name='accounts', on_delete=models.PROTECT, null=True, blank=True)
     opening_balance = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
     type : str = models.CharField(max_length=50, null=False, default='checking', choices=[('checking', 'Checking'), ('savings', 'Savings'), ('custody', 'Custody')])
 
